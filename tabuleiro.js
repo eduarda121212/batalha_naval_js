e9155103b8bbab433a2364901adaa953929099d1
eduarda <eduarda.n.silva11@aluno.senai.br>
2025-04-09 14:35:28 -0300

    duplajs

---
+++ tabuleiro.js
@@ -0,0 +1,24 @@
+/** O código fonte agora da acessa a Eduarda */
+//feito
+ 
+var Tabuleiro = [];
+
+for (let l = 0; l < 5; l++) {
+    Tabuleiro[l] = []; 
+    for (let c = 0; c < 5; c++) {
+        Tabuleiro[l][c] = false;
+    }
+}
+
+for(let i = 0; i < 5; i++){
+    let posx = Math.floor(Math.random() * 5);
+    let posy = Math.floor(Math.random() * 5);
+    Tabuleiro [posx][posy] = true;
+}
+
+
+
+
+
+
+console.log(Tabuleiro);
